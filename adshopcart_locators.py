dc444d134707a6a2e994cdcf4704d4b65cdde61e
Mayank Bhosale <rautgeeta@gmail.com>
2022-03-01 23:24:31 -0800

    Modified adshopcart_locators.py file

--- adshopcart_locators.py
+++ adshopcart_locators.py
@@ -1,2 +1,16 @@
+from faker import Faker
+
 adshopcart_url = 'https://advantageonlineshopping.com/#/'
 adshopcart_title = ' Advantage Shopping'
+fake = Faker(locale='en_CA')
+username = fake.user_name()
+password = fake.password()
+first_name = fake.first_name()
+last_name = fake.last_name()
+full_name = f'{first_name} {last_name}'
+phone = fake.phone_number()
+email = fake.email()
+city = fake.city()
+street_address = fake.street_address()
+province = fake.province_abbr()
+postal_code = fake.postalcode()
